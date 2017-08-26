4fa4e3cc7af7316d401422f9a897bc186395447e
Aram Anderson <aram.j.anderson@gmail.com>
2017-08-26 14:28:55 -0600

    Add article model, tests, and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create(title: "Michael!!!!", body: "It's called 'taking advantage.' It's what gets you ahead in life. I don't understand the question, and I won't respond to it. Really? Did nothing cancel? Steve Holt! Steve Holt!
+
+As you may or may not know, Lindsay and I have hit a bit of a rough patch. First place chick is hot, but has an attitude, doesn't date magicians. Not tricks, Michael, illusions. Army had half a day.
+
+Whoa, this guy's straight? Army had half a day. I don't understand the question, and I won't respond to it. That's what it said on 'Ask Jeeves.' Really? Did nothing cancel?
+
+Bad news. Andy Griffith turned us down. He didn't like his trailer. He'll want to use your yacht, and I don't want this thing smelling like fish. First place chick is hot, but has an attitude, doesn't date magicians.
+
+Really? Did nothing cancel? Now, when you do this without getting punched in the chest, you'll have more fun. Michael! But I bought a yearbook ad from you, doesn't that mean anything anymore? Across from where?
+
+I've opened a door here that I regret. Marry me. As you may or may not know, Lindsay and I have hit a bit of a rough patch. I'm a monster. Really? Did nothing cancel?
+
+Well, what do you expect, mother? Bad news. Andy Griffith turned us down. He didn't like his trailer. That's what it said on 'Ask Jeeves.' Now, when you do this without getting punched in the chest, you'll have more fun.
+
+I don't criticize you! And if you're worried about criticism, sometimes a diet is the best defense. Say goodbye to these, because it's the last time! We just call it a sausage. Oh, you're gonna be in a coma, all right.")
+
+Article.create(title: "Gob", body: "So maybe you could start jete-ing, and stop je-terrorizing me! I'll have a vodka rocks. (Mom, it's breakfast time.) And a piece of toast. I'll buy you a hundred George Michaels that you can teach to drive! Her lawyers are claiming the seal is worth $250,000. And that's not even including Buster's Swatch. Waiting for the Emmys. BTW did you know won 6 Emmys and was still canceled early by Fox? COME ON. Touché, Pandora. Why are you squeezing me with your body? Are you sure this isn't her sister? Mrs Veal: What a lovely thing to say. Michael: That's an awful thing to say.
+
+So, what do you say? We got a basket full of father-son fun here. What's Kama Sutra oil? Maybe it's not for us. No, it's the opposite. It's like my heart is getting hard.
+
+Okay, Lindsay, are you forgetting that I was a professional twice over - an analyst and a therapist. The world's first analrapist. I mean, it's one banana, Michael. What could it cost, ten dollars? No, I was ashamed to be SEEN with you. I like being with you. Oh…yeah…the guy in the…the $4,000 suit is holding the elevator for a guy who doesn't make that in three months. Come on!
+
+George Bush doesn't care about black puppets. Tobias Fünke costume. I am getting rid of this thing. It has caused me nothing but pride and self-respect. Stop it, stop it. This objectification of women has to stop. It's just Mom and whores. Can you believe that the only reason the club is going under is because it's in a terrifying neighborhood? They want to break his legs. It's a good thing he's already got that little scooter. A million ****ing diamonds! Friend of mine from college. He also has a boat tho not called the Seaward.")
+
+Article.create(title: "Lindsay", body: "Coo coo ca chaw. Coo coo ca chaw. Coo coo ca chaw. George Bush doesn't care about black puppets. Please refrain from discussing or engaging in any sort of interoffice [bleep] or [bleep] or finger[bleep] or [bleep]sting or [bleep] or even [bleep].
+
+I need a fake passport, preferably to France… I like the way they think. Never once touched my per diem. I'd go to Craft Service, get some raw veggies, bacon, Cup-A-Soup…baby, I got a stew goin'. Don't ask 'Can I'… ask 'I Can!' Yeah, like I'm going to spill coffee all over this $3,000 suit? Come on!
+
+[climbing under trampoline] This shall keep me safe from the hot Mexican sun. She calls it a mayonegg. Yes, he's like the steel man from The Wizard From Oz.")
