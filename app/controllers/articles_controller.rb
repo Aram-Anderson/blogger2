06e9953ac8663dd2d5461b0a324c07307a4c981f
Aram Anderson <aram.j.anderson@gmail.com>
2017-08-26 15:43:02 -0600

    Delete done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    redirect_to article_path(Article.create(article_params))
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to '/articles'
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
